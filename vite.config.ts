0097d69391d0f7baf7bd4e07b71dede8f37255ef
PHUNG ANH TUAN <tuan29102005@gmail.com>
2026-03-11 19:06:04 +0700

    tauri integration

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,35 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
+const host = process.env.TAURI_DEV_HOST
+
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(async () => ({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  // 1. prevent vite from obscuring rust errors
+  clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: "ws",
+          host,
+          port: 5174,
+        }
+      : undefined,
+    watch: {
+      // 3. tell vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"],
+    },
+  },
+}))
